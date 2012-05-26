3c8161fde5fd8981b8a0090e3146756f5a578229
Dmitry.Buzdin <buzdin@gmail.com>
2012-05-26 12:33:40 +0300

    Generated initial project structure;

---
+++ src/eu/hack4europe/postcard/PostcardActivity.java
@@ -0,0 +1,15 @@
+package eu.hack4europe.postcard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostcardActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
