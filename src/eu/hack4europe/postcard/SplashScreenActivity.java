f5084121255f28427eba1d1e0b27ae2216326313
Alexey <alex.buzdin@gmail.com>
2012-05-27 10:51:31 +0300

    Splashscreen fixed

---
+++ src/eu/hack4europe/postcard/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package eu.hack4europe.postcard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splashscreen);
+	
+		final int welcomeScreenDisplay = 3000;
+		Thread welcomeThread = new Thread() {
+
+			int wait = 0;
+
+			@Override
+			public void run() {
+				try {
+					super.run();
+					while (wait < welcomeScreenDisplay) {
+						sleep(100);
+						wait += 100;
+					}
+				} catch (Exception e) {
+					System.out.println("EXc=" + e);
+				} finally {
+					 startActivity(new Intent(SplashScreenActivity.this,
+					 PostcardActivity.class));
+					finish();
+				}
+			}
+		};
+		welcomeThread.start();
+
+	}
+}
